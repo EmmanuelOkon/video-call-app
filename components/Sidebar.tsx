fd11039e8ca42332d2af24b495c1dbd40b9d0386
Emmanuel Okon <emmaudo27@gmail.com>
2024-04-21 21:09:41 +0100

    set up layout and routers

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,39 @@
+"use client"
 
+import { cn } from "@/lib/utils";
+import { sidebarLinks } from "@/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname();
+  // const
   return (
-    <div>Sidebar</div>
-  )
-}
+    <section className="sticky left-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px] ">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            pathname === link.route || pathname.startsWith(link.route);
+          console.log(pathname);
 
-export default Sidebar
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              className={cn(
+                "flex gap-4 items-center p-4 rounded-lg justify-start",
+                {
+                  "bg-blue-1": isActive,
+                }
+              )}
+            >
+              {link.label}
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Sidebar;
