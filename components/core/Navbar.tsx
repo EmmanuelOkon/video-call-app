b4977165ff21d456cd7ad0bfd6d843709c4376a4
Emmanuel Okon <emmaudo27@gmail.com>
2024-04-22 15:36:08 +0100

    created sidebar component and mobilenav component

---
+++ components/core/Navbar.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import MobileNav from "./MobileNav";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:py-8">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="Voom logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden pl-2">
+          Voom
+        </p>
+      </Link>
+
+      <div className="flex-between gap-5 ">
+        {/* Clerk */}
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- utils/index.ts
+++ utils/index.ts
@@ -1,27 +1,30 @@
+// import { Home } from "lucide-react";
+
 export const sidebarLinks = [
-    {
-        label: "Home",
-        route: "/",
-        imgUrl: "/icons/home.svg"
-    },
-    {
-        label: "Upcoming",
-        route: "/upcoming",
-        imgUrl: "/icons/upcoming.svg"
-    },
-    {
-        label: "Previous Meetings",
-        route: "/previous-meetings",
-        imgUrl: "/icons/previous.svg"
-    },
-    {
-        label: "Recordings",
-        route: "/recordings",
-        imgUrl: "/icons/video.svg"
-    },
-    {
-        label: "Personal Room",
-        route: "/personal-room",
-        imgUrl: "/icons/add-personal.svg"
-    },
-]
+  {
+    label: "Home",
+    route: "/",
+    imgUrl: "/icons/home.svg"
+    // imgUrl: Home,
+  },
+  {
+    label: "Upcoming Meetings",
+    route: "/upcoming",
+    imgUrl: "/icons/upcoming.svg",
+  },
+  {
+    label: "Previous Meetings",
+    route: "/previous-meetings",
+    imgUrl: "/icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    imgUrl: "/icons/video.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    imgUrl: "/icons/add-personal.svg",
+  },
+];
