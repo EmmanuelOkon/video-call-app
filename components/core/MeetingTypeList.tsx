4a8263de5336d9369993b8a506dadc17701f1603
Emmanuel Okon <emmaudo27@gmail.com>
2024-04-23 16:36:52 +0100

    created stream client keys for calls

---
+++ components/core/MeetingTypeList.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import React, { useState } from "react";
+import HomeCard from "./HomeCard";
+import { useRouter } from "next/navigation";
+import MeetingModal from "./MeetingModal";
+
+const MeetingTypeList = () => {
+    const router = useRouter();
+    const [meetingState, setMeetingState] = useState<
+      "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+    >(undefined);
+
+
+    const createMeeting = () => {
+
+
+    }
+
+
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        alt="add"
+        className="bg-orange-1"
+        handleClick={() => setMeetingState("isInstantMeeting")}
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="via invitation link"
+        alt="add person"
+        className="bg-blue-1"
+        handleClick={() => setMeetingState("isJoiningMeeting")}
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        alt="calendar"
+        className="bg-purple-1"
+        handleClick={() => setMeetingState("isScheduleMeeting")}
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Meeting Recordings"
+        alt="record"
+        className="bg-yellow-1"
+        handleClick={() => router.push("/recordings")}
+      />
+
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+      />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
