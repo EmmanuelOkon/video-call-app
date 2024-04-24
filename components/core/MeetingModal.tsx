ef24c8aec19ba55d4a882089c02a1fac9860986c
Emmanuel Okon <emmaudo27@gmail.com>
2024-04-24 04:26:20 +0100

    setup stream for calls by authenticating user id

--- components/core/MeetingModal.tsx
+++ components/core/MeetingModal.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 import { ReactNode } from "react";
-import { Dialog, DialogContent } from "../ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
 import Image from "next/image";
@@ -33,9 +41,11 @@ const MeetingModal = ({
   buttonClassName,
   buttonIcon,
 }: MeetingModalProps) => {
+  console.log("isOpen:", isOpen); // Log isOpen prop value
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogTrigger>Open</DialogTrigger>
       <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
         <div className="flex flex-col gap-6">
           {image && (
--- components/core/MeetingTypeList.tsx
+++ components/core/MeetingTypeList.tsx
@@ -1,22 +1,71 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { useToast } from "../ui/use-toast";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 
 const MeetingTypeList = () => {
-    const router = useRouter();
-    const [meetingState, setMeetingState] = useState<
-      "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
-    >(undefined);
+  const router = useRouter();
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >(undefined);
 
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  });
+  const [callDetail, setCallDetail] = useState<Call>();
+  const { toast } = useToast();
+  const client = useStreamVideoClient();
+  const { user } = useUser();
 
-    const createMeeting = () => {
+  const createMeeting = async () => {
+    if (!client || !user) return;
 
+    try {
+      if (!values.dateTime) {
+        toast({ title: "Please select a date and time" });
+        return;
+      }
+      const id = crypto.randomUUID();
 
+      const call = client.call("default", id);
+
+      if (!call) throw new Error("Failed to create call");
+
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+
+      const description = values.description || "Instant Meeting";
+
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+
+      setCallDetail(call);
+
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+
+      toast({title: "Meeting created"})
+    } catch (error) {
+      console.log(error);
+      toast({ title: "Failed to create Meeting" });
     }
+  };
 
+  console.log(meetingState);
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
